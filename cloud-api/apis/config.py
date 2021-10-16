fca3d63b99899e866b0f06ffca9a1d78d049c016
Towfiq <towfiqomi@gmail.com>
2021-10-16 18:22:46 +0300

    swagger details change

---
+++ cloud-api/apis/config.py
@@ -0,0 +1,3 @@
+API_VERSION = "1.0.0"
+API_DESCRIPTION ="Aiven Interview Take Home Task API"
+API_NAME = "cloud-api"
--- cloud-api/apis/main.py
+++ cloud-api/apis/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
+from apis import config
+
+
+def application_details() -> FastAPI:
+    application = FastAPI(
+        title=config.API_NAME,
+        description=config.API_DESCRIPTION,
+        version=config.API_VERSION,
+    )
+    return application
+
+app = application_details()
+
 
 @app.get("/")
 def hello_world():
