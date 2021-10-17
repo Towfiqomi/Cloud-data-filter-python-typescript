633b21ce1f04ef4775304d47ad9867b09297a344
Towfiq <towfiqomi@gmail.com>
2021-10-17 10:42:11 +0300

    model unit test added

---
+++ cloud-api/tests/test_cloud_list_model.py
@@ -0,0 +1,13 @@
+from unittest import mock
+import pytest
+from apis.main import app
+from apis.common import dataprovider
+from tests import fixtures
+
+@pytest.mark.asyncio
+@mock.patch("apis.common.dataprovider.get_cloud_list")
+async def test__model_get_cloud_list_success(mock_model_get_cloud_list : mock.Mock):
+    MOCK_CLOUD_LIST = fixtures.CloudList
+    mock_model_get_cloud_list.return_value = MOCK_CLOUD_LIST
+    response = await dataprovider.get_cloud_list()
+    assert response == MOCK_CLOUD_LIST
--- cloud-api/tests/test_dataprovider.py
+++ cloud-api/tests/test_dataprovider.py
@@ -5,7 +5,7 @@ from apis.common import dataprovider
 
 @pytest.mark.asyncio
 @mock.patch("apis.common.dataprovider.get_cloud_list")
-async def test_data_proovider_get_cloud_list_success(mock_dataprovider_get_cloud_list : mock.Mock):
+async def test_data_provider_get_cloud_list_success(mock_dataprovider_get_cloud_list : mock.Mock):
     MOCK_CLOUD_LIST = fixtures.CloudList
     mock_dataprovider_get_cloud_list.return_value = MOCK_CLOUD_LIST
     response = await dataprovider.get_cloud_list()
@@ -13,7 +13,7 @@ async def test_data_proovider_get_cloud_list_success(mock_dataprovider_get_cloud
 
 @pytest.mark.asyncio
 @mock.patch("apis.common.dataprovider.get_cloud_list")
-async def test_data_proovider_get_cloud_list_failed(mock_dataprovider_get_cloud_list : mock.Mock):
+async def test_data_provider_get_cloud_list_failed(mock_dataprovider_get_cloud_list : mock.Mock):
     mock_exception = Exception(
             "Cloud list from public api is not served"
         )
