1f82c09ef6370e12464a5f2a657833cf473dfe1b
Towfiq <towfiqomi@gmail.com>
2021-10-23 20:42:00 +0300

    test updated

--- cloud-api/tests/test_cloud_list_model.py
+++ cloud-api/tests/test_cloud_list_model.py
@@ -5,9 +5,9 @@ from apis.common import dataprovider
 from tests import fixtures
 
 @pytest.mark.asyncio
-@mock.patch("apis.common.dataprovider.get_cloud_list")
-async def test__model_get_cloud_list_success(mock_model_get_cloud_list : mock.Mock):
+@mock.patch("apis.common.dataprovider.get_paginated_cloud_list")
+async def test__model_get_cloud_list_success(mock_model_get_paginated_cloud_list : mock.Mock):
     MOCK_CLOUD_LIST = fixtures.CloudList
-    mock_model_get_cloud_list.return_value = MOCK_CLOUD_LIST
-    response = await dataprovider.get_cloud_list()
+    mock_model_get_paginated_cloud_list.return_value = MOCK_CLOUD_LIST
+    response = await dataprovider.get_paginated_cloud_list()
     assert response == MOCK_CLOUD_LIST
--- cloud-api/tests/test_cloud_list_view.py
+++ cloud-api/tests/test_cloud_list_view.py
@@ -8,9 +8,9 @@ from tests import fixtures
 client = TestClient(app)
 
 @pytest.mark.asyncio
-@mock.patch("apis.models.cloud_list.get_cloud_list")
-async def test_get_cloud_list_success(mock_model_get_cloud_list : mock.Mock):
+@mock.patch("apis.models.cloud_list.get_paginated_cloud_list")
+async def test_get_cloud_list_success(mock_model_get_paginated_cloud_list : mock.Mock):
     MOCK_CLOUD_LIST = fixtures.CloudList
-    mock_model_get_cloud_list.return_value = MOCK_CLOUD_LIST
-    response = await cloud_list.cloud_list.get_cloud_list()
+    mock_model_get_paginated_cloud_list.return_value = MOCK_CLOUD_LIST
+    response = await cloud_list.get_paginated_cloud_list(current_page=1, page_size=10)
     assert response == MOCK_CLOUD_LIST
